a58d7a1bfa7b6876f0f81cde7a818b27374eafc6
S. Rusi <sararusi111@gmail.com>
2024-11-22 14:03:53 +0000

    Update listops.py

--- listops.py
+++ listops.py
@@ -1,6 +1,6 @@
-lists = [12, 23, 42, 55, 46, 76, 56, 88, 87, 66]
+lists = [12,23,42,55,46,76,56,88,87,66]
 
-print("The list is:", (list))
+print("The list is:", list)
 print("The sum of the list is:", sum(list))
 print("The largest number of the list is:", max(list))
 print("The list in reverse is:", list[::-1])
