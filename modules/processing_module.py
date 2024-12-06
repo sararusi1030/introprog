afcef4791ef3ca489e7307726b41d317418599df
Sara Rusi <sararusi111@gmail.com>
2024-12-06 13:51:57 +0000

    added code assessment 9

---
+++ modules/processing_module.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    return "undefined"  # To handle division by zero
