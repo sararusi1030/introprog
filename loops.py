750516a19203b77dd0c691372de6d15b11d2b360
Sara G. <sararusi111@gmail.com>
2025-04-25 23:15:22 +0100

    deleted old code

--- loops.py
+++ loops.py
@@ -1,10 +1,1 @@
-n = int(input("Enter a non negative integer: "))
 
-if n < 0:
-    print("Error: Integer entered is negative")
-else:
-    factorial = 1
-
-for x in range(1, n + 1):
-    factorial = factorial * x
-    print(x, ":", factorial)
