a99f911d859d2b4e97fd00b248f229d37949064b
Sara G. <sararusi111@gmail.com>
2025-04-25 23:15:32 +0100

    deleted old code

--- listops.py
+++ listops.py
@@ -1,7 +1,1 @@
-lists = [12,23,42,55,46,76,56,88,87,66]
 
-print("The list is:", lists)
-print("The sum of the list is:", sum(lists))
-print("The largest number of the list is:", max(lists))
-print("The list in reverse is:", lists[::-1])
-print("The smallest number of the list is:", min(lists))
