1fab99e2ddfd46d6e94cf492b84edb65341e4a23
Sara G. <sararusi111@gmail.com>
2025-04-25 23:14:56 +0100

    deleted old code

--- grades.py
+++ grades.py
@@ -1,19 +1,1 @@
-def determine_grades(mark):
-    if mark >= 70:
-        return "A"
-    elif mark >= 60:
-        return "B"
-    elif mark >= 50:
-        return "C"
-    elif mark >= 40:
-        return "D"
-    elif mark >= 30:
-        return "E"
-    elif mark >= 20:
-        return "F"
-    else:
-        return "Please enter a valid number between 20 and 100"
 
-mark = float(input("Enter the student's grade: "))
-
-print(determine_grades(mark))
